9400ab0d3ff19ac2e560fd879333507a0a2bfa17
markrusselldev <mark@markrussell.dev>
2022-12-11 18:37:52 -0700

    feat: begin playRound()
    make playerSelection lowercase

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
 // Don't reinvent the wheel.
+// getRandomInt(3); in getComputerChoice() - expected output: 0, 1 or 2
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -8,9 +9,20 @@ function getRandomInt(max) {
 // Get the array value where index equals random number
 // Return computer choice
 function getComputerChoice() {
-  let choices = ["Rock", "Paper", "Scissors"];
-  let randomInt = getRandomInt(3); // expected output: 0, 1 or 2
+  let choices = ["rock", "paper", "scissors"];
+  let randomInt = getRandomInt(3);
   return choices[randomInt];
 }
 
-console.log(getComputerChoice());
+//console.log(getComputerChoice());
+
+// Make playerSelection lowercase (computerSelection is already lowercase)
+// Compare playerSelection with computerSelection
+// IF
+
+function playRound(playerSelection, computerSelection) {
+  let lowercasePlayerSelection = playerSelection.toLowerCase();
+  return lowercasePlayerSelection;
+}
+
+//console.log(playRound("ROCK", "scissors"));
