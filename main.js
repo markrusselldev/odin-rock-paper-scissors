07acd7b35613ac8e939e2f5a52030622cd109db3
markrusselldev <mark@markrussell.dev>
2022-12-11 18:13:31 -0700

    plan: complete pseudocode for getComputerChoice()

---
+++ main.js
@@ -0,0 +1,5 @@
+// Create an array holding our "Rock", "Paper", "Scissors" values
+// Generate a random number between 0 and 2
+// Get the array value where index equals random number
+// Return computer choice
+function getComputerChoice() {}
